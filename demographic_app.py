aff96378a7486bdf97a3f9eefcb20240b9ffd322
César Martínez <cesar.mart@gmail.com>
2020-12-20 18:03:26 -0300

    subida 13

--- demographic_app.py
+++ demographic_app.py
@@ -19,29 +19,29 @@ if uploaded_file is not None:
 
     st.image(uploaded_file, caption='Imagen analizada', use_column_width=True)
 
-    r = requests.post(
-        "https://api.deepai.org/api/demographic-recognition",
-        files={
-            'image': uploaded_file,
-        },
-        headers={'api-key': 'ed22d0b2-4cc5-4223-9e48-302f8a86c7c5'}
-    )
+    #r = requests.post(
+        #"https://api.deepai.org/api/demographic-recognition",
+        #files={
+            #'image': uploaded_file,
+        #},
+        #headers={'api-key': 'ed22d0b2-4cc5-4223-9e48-302f8a86c7c5'}
+    #)
 
-    response_data = r.json()
-    resultado = response_data['output']['faces'][0]
-    #st.text("Resultado: {}.".format(resultado))
+    #response_data = r.json()
+    #resultado = response_data['output']['faces'][0]
+    ##st.text("Resultado: {}.".format(resultado))
     
-    age = resultado['age_range']
-    st.text("Su edad está en el rango: {}-{} años.".format(age[0],age[1])) 
+    #age = resultado['age_range']
+    #st.text("Su edad está en el rango: {}-{} años.".format(age[0],age[1])) 
 
-    gender = resultado['gender']
-    if gender=='Male':
-        st.text("Ud. es hombre.")
-    else:
-	    st.text("Ud. es mujer.")
+    #gender = resultado['gender']
+    #if gender=='Male':
+        #st.text("Ud. es hombre.")
+    #else:
+	    #st.text("Ud. es mujer.")
 
-    demography = resultado['cultural_appearance']
-    st.text("Su apariencia demográfica es: {}.".format(demography))
+    #demography = resultado['cultural_appearance']
+    #st.text("Su apariencia demográfica es: {}.".format(demography))
 
 
     r2 = requests.post("https://api.deepai.org/api/facial-expression-recognition",
